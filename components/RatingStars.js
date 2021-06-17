a6a8c40591144eb8da00a0240f04193babea8ef5
Damian Piętka <damianpietka@gmail.com>
2021-06-17 19:16:42 +0200

    comments

--- components/RatingStars.js
+++ components/RatingStars.js
@@ -4,24 +4,27 @@ import { Ionicons } from '@expo/vector-icons';
 
 const RatingStars = ({ rating }) => {
   const floored = Math.floor(rating);
-  const halfStar = rating - floored;
+  const halfStar = rating - floored; // 4.51 - 4 = 0.51
 
   const getStars = () => {
     const stars = [];
-    let emptyStar = 5;
+    let emptyStar = 5; // decrease if full stars or half stars are pushed to array
     let key = 0;
+    // full stars
     for (let i = 0; i < floored; i++) {
       emptyStar -= 1;
       stars.push(
         <Ionicons key={key++} name='star-sharp' size={20} color='yellow' />
       );
     }
+    // half stars
     if (halfStar >= 0.5) {
       stars.push(
         <Ionicons key={key++} name='star-half-sharp' size={20} color='yellow' />
       );
       emptyStar -= 1;
     }
+    // empty stars
     for (let i = 0; i < emptyStar; i++) {
       stars.push(
         <Ionicons key={key++} name='star-outline' size={20} color='yellow' />
