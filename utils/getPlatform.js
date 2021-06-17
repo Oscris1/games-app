84a87d7ef633a1d1effeb4a2a2c1af2728e3f7ef
Damian Piętka <damianpietka@gmail.com>
2021-06-17 18:44:00 +0200

    created getPlatform utils function to get platform icon

---
+++ utils/getPlatform.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import { MaterialIcons } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
+import { FontAwesome5 } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+export const getPlatform = (name) => {
+  const size = 25;
+  //const color = '#64ffda';
+  const color = '#fff';
+  if (name === 'PC')
+    return <MaterialIcons name='computer' size={size} color={color} />;
+  else if (name === 'PlayStation 4' || name === 'PlayStation 5')
+    return <FontAwesome5 name='playstation' size={size} color={color} />;
+  else if (name === 'Xbox Series S/X' || name === 'Xbox One')
+    return <FontAwesome5 name='xbox' size={size} color={color} />;
+  else if (name === 'Nintendo Switch')
+    return (
+      <MaterialCommunityIcons
+        name='nintendo-switch'
+        size={size}
+        color={color}
+      />
+    );
+  else if (name === 'iOS')
+    return <FontAwesome5 name='apple' size={size} color={color} />;
+  else return <FontAwesome name='gamepad' size={size} color={color} />;
+};
